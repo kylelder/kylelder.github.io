da6072436aeec0abae6332972a7d35b162c6cb24
Zach Drago <zacharydrago@MacBook-Pro-3.local>
2016-10-10 10:34:33 -0700

    initial

---
+++ js/js.js
@@ -0,0 +1,170 @@
+// ADD MILES LIGHTBOX
+function addMiles() {
+    $('#lightbox').lightbox_me({
+        appearEffect: 'fadeIn',
+        centered: false,
+        showOverly: true,
+        overlayCSS: {background: '#2e3641', opacity: 0.9},
+        closeClick: true,
+        lightboxSpeed: 'fast',
+        modalCSS: {top: '15%'},
+        closeSelector: ".close",
+    });
+}
+
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn') && !event.target.matches('#searchInput')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+
+// function formName(id) {
+//     $('.dropbtn').val(id);
+//     // alert(id);
+// }
+
+
+function formName(id) {
+    $('.dropbtn').val(id);
+    $('.milesInput').attr('name', id);
+    // alert(id);
+}
+
+
+// INSTAFEED ======================================|
+var userFeed = new Instafeed({
+  get: 'user',
+  userId: '4011766578',
+  clientId: '0b08ac92d16b4248b68c90805daa37d6',
+  accessToken: '4011766578.1677ed0.a5b70da1e603486490c8bdbb2ca6e875',
+  resolution: 'standard_resolution',
+  template: '<div id="{{image}}" class="insta_pic" onclick="instaPop(this.id, this.link)" style="background: url({{image}}) center center;"></div>'
+});
+userFeed.run(); 
+
+
+function instaPop(id, link) {
+
+    var insta_large = $('.insta_large');
+    insta_large.attr('src', id);
+
+    var insta_link = $('.insta_link');
+    insta_link.attr('href', link);
+    
+
+    $('#instaPop').lightbox_me({
+        appearEffect: 'fadeIn',
+        centered: true,
+        showOverly: true,
+        overlayCSS: {background: '#2e3641', opacity: 0.9},
+        closeClick: true,
+        lightboxSpeed: 'fast',
+    });
+    //e.preventDefault();
+    //e.stopPropagation();
+}
+
+
+
+// COVER VIDEO ======================================|
+$( document ).ready(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    // console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
+}
+
+
+
+
+
+
---
+++ js/sheets.js
@@ -0,0 +1,181 @@
+// Variable to hold request
+var request;
+
+// Bind to the submit event of our form
+$("#miles").submit(function(event){
+
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        // url: "https://script.google.com/macros/s/AKfycbwzJmm6HwkWvi0uXRxl9deJ8z6pJbIGeAWr6YQGgstbQ17hM49N/exec",
+        url: "https://script.google.com/macros/s/AKfycbwzJmm6HwkWvi0uXRxl9deJ8z6pJbIGeAWr6YQGgstbQ17hM49N/exec",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+        console.log(response);
+        console.log(textStatus);
+        console.log(jqXHR);
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+
+    $('.dropbtn').val();
+    $('.milesInput').val();
+    var dropbtn = document.getElementById('name');
+    dropbtn.value = '';
+    var milesInput = document.getElementById('milesRan');
+    milesInput.value = '';
+
+    clearMileName();
+
+    // Prevent default posting of form
+    event.preventDefault();
+});
+
+function clearMileName() {
+    $('.milesInput').attr('name', "name");
+    $('.submitBtn').hide();
+    $('.thanks').show();   
+    
+    setTimeout(function(){
+        $('.submitBtn').show();
+        $('.thanks').hide();
+    }, 5000);
+}
+
+
+
+
+
+// GET TOTAL MILES RAN FROM SHEET ==================================
+
+window.onload = function() {
+  $('.total-submit').click();
+  $('.runners-total-submit').click();
+};
+
+function getTotals() {
+  $('.total-submit').click();
+  $('.runners-total-submit').click();
+};
+
+
+
+
+
+//Prevent Default Form Behavior (don't want the form submit to re-load the page)
+$(document).ready(function() {
+  $('#totals').submit(function(evt) {
+    evt.preventDefault();
+  });
+});
+
+var googleSheetKey = "1jsaOqUEUFo6Y5wSVvn0JGY57nlpcp_KzCjK2nF9AwH0";
+var theSheet;//Save the preloaded JSON object here
+
+//pre-load the sheet on page load
+$.ajax({
+  url: 'https://spreadsheets.google.com/feeds/list/' + googleSheetKey + '/o4yaqwc/public/values?alt=json-in-script',
+  dataType: 'jsonp',
+  success: function(dataWeGotViaJsonp) {
+    theSheet = dataWeGotViaJsonp;
+  }
+});
+
+
+function populatePage() {
+  var totalMiles = "";
+  theSheet.feed.entry.forEach(
+    function(row) {
+      totalMiles = row.gsx$total.$t;   
+    }
+  );
+  $('#totalMiles').html(totalMiles);
+  return false;
+}
+
+
+
+// ==============
+
+//Prevent Default Form Behavior (don't want the form submit to re-load the page)
+$(document).ready(function() {
+  $('#runners-totals').submit(function(evt) {
+    evt.preventDefault();
+  });
+});
+
+var SheetKey = "1jsaOqUEUFo6Y5wSVvn0JGY57nlpcp_KzCjK2nF9AwH0";
+var theSheet2;//Save the preloaded JSON object here
+
+//pre-load the sheet on page load
+$.ajax({
+  url: 'https://spreadsheets.google.com/feeds/list/' + SheetKey + '/o4gp47q/public/values?alt=json-in-script',
+  dataType: 'jsonp',
+  success: function(dataWeGotViaJsonp) {
+    theSheet2 = dataWeGotViaJsonp;
+  }
+});
+  
+  
+function populateTable() {
+  table = "";
+  theSheet2.feed.entry.forEach(
+    function(row) {
+      tableRow = "<tr>";
+      tableRow += "<td>" + row.gsx$name.$t + "</td>";
+      tableRow += "<td>" + row.gsx$total.$t + "</td>";
+      tableRow += "</tr>";
+      table += tableRow;
+    }
+  );
+  $('#individual-miles').html("<tbody id='runner-list'><tr><th>runner</th><th>miles</th></tr>" + table + "</tbody>");
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
